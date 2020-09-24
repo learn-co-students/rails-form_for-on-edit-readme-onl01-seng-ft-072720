49cbc366232169c9ef5e82b5d284c4f4774a7d24
HuffDaddy0 <ty.huff24@gmail.com>
2020-09-24 13:49:44 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
 
 	def show
 		@post = Post.find(params[:id])
+
 	end
 
 	def new
@@ -25,7 +26,8 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
-	  redirect_to post_path(@post)
+	  @post.update(params.require(:post).permit(:title, :description))
+		redirect_to post_path(@post)
+	 
 	end
 end
